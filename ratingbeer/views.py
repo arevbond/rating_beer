232f13732af3127369770ad6650c44345c3a050b
arevbond <bondarev.n17@yandex.ru>
2022-09-24 15:49:53 +0300

    change: add new pole db

--- ratingbeer/views.py
+++ ratingbeer/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def index(request):
+    posts = Beer.objects.all()
+    return render(request, 'ratingbeer/index.html', {'posts': posts})
+
