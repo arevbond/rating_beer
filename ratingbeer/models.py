6b7ef45f8f3bd9f6850795741bdde7efb3cc5ae3
arevbond <bondarev.n17@yandex.ru>
2022-09-24 12:41:58 +0300

    fix: fix mistakes

--- ratingbeer/models.py
+++ ratingbeer/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class Beer(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    country = models.ForeignKey(Country, on_delete=models.PROTECT())
-    image = models.ImageField(upload_to='images/%Y/%m/%d', height_field=200, width_field=200)
-    slug = models.SlugField()
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
-
 class Country(models.Model):
     title = models.CharField(max_length=200)
 
@@ -27,6 +11,22 @@ class Country(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=200)
     desc = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Beer(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='images/%Y/%m/%d', height_field=200, width_field=200)
+    slug = models.SlugField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
