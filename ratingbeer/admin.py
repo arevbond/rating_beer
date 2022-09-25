8e430586faddd9c7cef20759150531b8fddb4e41
arevbond <bondarev.n17@yandex.ru>
2022-09-25 15:13:56 +0300

    change: admin panel

--- ratingbeer/admin.py
+++ ratingbeer/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 
+from .models import *
+
+
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_title', 'short_description', 'price', 'alcogol', 'category', 'country',
+                    'image', 'time_create', 'is_published')
+
+    list_display_links = ('id', 'short_title')
+    list_editable = ('is_published', )
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Beer, BeerAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Country)
--- ratingbeer/models.py
+++ ratingbeer/models.py
@@ -1,25 +1,41 @@
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import truncatechars
 
 
 class Beer(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.PositiveIntegerField()
-    alcogol = models.FloatField(default=5)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-    country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True)
-    image = models.ImageField(upload_to='media/%Y/%m/%d', height_field=200, width_field=200)
-    slug = models.SlugField()
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    alcogol = models.FloatField(default=5, verbose_name='Алкоголь')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True,
+                                 verbose_name='Категория')
+    country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True,
+                                verbose_name='Страна')
+    image = models.ImageField(upload_to='media/%Y/%m/%d', height_field=200, width_field=200,
+                              verbose_name='Фото')
+    slug = models.SlugField(verbose_name='Slug')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+
+    @property
+    def short_title(self):
+        return truncatechars(self.title, 20)
+    @property
+    def short_description(self):
+        return truncatechars(self.description, 35)
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Пиво'
+        verbose_name_plural = 'Пиво'
+        ordering = ['id']
 
 
 class Country(models.Model):
@@ -28,10 +44,20 @@ class Country(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страна'
+        ordering = ['title']
+
 
 class Category(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Категория')
     desc = models.TextField()
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
